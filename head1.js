b327cf413701635cc945af805ad9ece13c8f893c
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-03-17 17:36:03 -0700

    Create head1.js

---
+++ head1.js
@@ -0,0 +1,14 @@
+const main = (size) =>
+    cube([10,10,1])
+    .translate([-10,-5, -.5])
+    .union(
+        cube([20, 1, 1])
+        .translate([0, 1.5, -.5])
+        .rotateZ(45)
+    )
+    .union(
+        cube([20, 1, 1])
+        .translate([0, -2.5, -0.5])
+        .rotateZ(-45)
+    )
+    .subtract(sphere(3));
